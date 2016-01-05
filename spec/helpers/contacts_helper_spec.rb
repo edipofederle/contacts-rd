87cb3bb08806cb2d6f904d64ec7fb86976c00997
Edipo L Federle <edipofederle@gmail.com>
2016-01-05 14:15:47 -0200

    minor

--- spec/helpers/contacts_helper_spec.rb
+++ spec/helpers/contacts_helper_spec.rb
@@ -2,16 +2,15 @@ require 'rails_helper'
 
 RSpec.describe ContactsHelper, type: :helper do
   it {
-    custom_field       = Fabricate(:custom_field)
+    custom_field = Fabricate(:custom_field)
 
-    contact = Fabricate(:contact, contact_fields: {company: 10})
+    contact      = Fabricate(:contact, contact_fields: {company: 10})
     expect(helper.field_content(contact, custom_field)).to eq 10
   }
 
   it {
-    custom_field       = Fabricate(:custom_field_combo)
-
-    contact = Fabricate(:contact, contact_fields: {company: 10})
+    custom_field = Fabricate(:custom_field_combo)
+    contact      = Fabricate(:contact, contact_fields: {company: 10})
     expect(helper.field_content(contact, custom_field)).to eq '-'
   }
 end
