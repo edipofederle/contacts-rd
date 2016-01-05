671fbb8b705b23b0b9dab3b3789a768a3aa98c56
Edipo L Federle <edipofederle@gmail.com>
2016-01-05 14:00:35 -0200

    Show custom fields for all contact, even if there is no value

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,2 +1,6 @@
 module ContactsHelper
+  def field_content(contact, field)
+    value = contact.contact_fields[field.name.downcase.to_sym]
+    value.blank? ? '-' : value
+  end
 end
--- spec/helpers/contacts_helper_spec.rb
+++ spec/helpers/contacts_helper_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ContactsHelper, type: :helper do
+  it {
+    custom_field       = Fabricate(:custom_field)
+
+    contact = Fabricate(:contact, contact_fields: {company: 10})
+    expect(helper.field_content(contact, custom_field)).to eq 10
+  }
+
+  it {
+    custom_field       = Fabricate(:custom_field_combo)
+
+    contact = Fabricate(:contact, contact_fields: {company: 10})
+    expect(helper.field_content(contact, custom_field)).to eq '-'
+  }
 end
